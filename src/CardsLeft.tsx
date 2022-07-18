438df8173086f48b1e2ffb43667d8fdcd171b757
cleionesio <cleionesio@hotmail.com>
2022-07-18 13:44:33 -0300

    first commit

---
+++ src/CardsLeft.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import './style.css';
+
+export default function CardsLeft() {
+  return (
+    <div>
+        <img src="https://media-exp1.licdn.com/dms/image/D4D35AQG3p5dW9bqLOQ/profile-framedphoto-shrink_200_200/0/1649935864164?e=1658354400&v=beta&t=NgsY3HOarCRAEnxSKZr9X8MjSo5plJWQjuSpdEQGNkw" alt="avatar" className="avatar" />
+
+       
+       
+      <div className="topico-left">
+      <h4>Contato</h4>
+       <p> Lagoa Santa - MG, Brasil</p>
+       <p>+55 (31) 99441-9870</p>
+       <p> <a href="mailto:cleionesio@gmail.com">Email</a></p>
+       <p><a href="https://www.linkedin.com/in/cleionesio-ribeiro-992615163/">LinkDin </a></p>
+
+       <p><a href="https://www.linkedin.com/in/cleionesio-ribeiro-992615163/">Git Hub </a></p>
+        
+        
+      </div>
+
+      <div className="topicos">
+      
+      </div>
+    </div>
+  );
+}
---
+++ src/CardsRigth.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import './style.css';
+
+export default function CardsRigth() {
+  return (
+    <div className="title">
+      <h1>Cleionésio Ribeiro</h1>
+      <div className="line"></div>
+
+      <div className="sub-title">
+      Desenvolvedor Web  front-end e back-end
+      </div>
+
+      <div className="topicos">
+      <h4> Objetivo profissional</h4>
+       <p> Em busca de um primeiro emprego como desenvolvedor web, front-end ou back-end</p>
+      </div>
+
+      <div className="topicos">
+      <h4>Disponibilidade</h4>
+       <p>Trabalho remoto, ou freelancer</p>
+      </div>
+
+      <div className="topicos">
+      <h4> Conhecimentos e  Habilidades</h4>
+       <p> Java, JavaScript, Node JS, SQL</p>
+       <p>HTML, CSS, Spring Boot, JSF, React, Angular, MYSQL, Git, GitHub</p>
+      </div>
+
+      <div className="topicos">
+      <h4> formação acadêmica</h4>
+       <p> Ensino médio completo</p>
+       <p> Autodidata em desenvolvimeto de software</p>
+       <p>Inglês básico</p>
+      </div>
+    </div>
+
+    
+  );
+}
