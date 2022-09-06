a222ed3a5fcd5c1b2924dfcac4272e23b6d0ceeb
Cleionesio_Ribeiro <58114878+Cleionesio2020@users.noreply.github.com>
2022-09-06 17:16:55 -0300

    atualizado card left

--- src/CardsLeft.tsx
+++ src/CardsLeft.tsx
@@ -4,7 +4,7 @@ import './style.css';
 export default function CardsLeft() {
   return (
     <div>
-        <img src="https://media-exp1.licdn.com/dms/image/D4D35AQG3p5dW9bqLOQ/profile-framedphoto-shrink_200_200/0/1649935864164?e=1658354400&v=beta&t=NgsY3HOarCRAEnxSKZr9X8MjSo5plJWQjuSpdEQGNkw" alt="avatar" className="avatar" />
+        <img src="https://avatars.githubusercontent.com/u/58114878?s=400&u=9764f30966db1d246389e3db85667dab1acb88e5&v=4" alt="avatar" className="avatar" />
 
        
        
@@ -12,10 +12,14 @@ export default function CardsLeft() {
       <h4>Contato</h4>
        <p> Lagoa Santa - MG, Brasil</p>
        <p>+55 (31) 99441-9870</p>
-       <p> <a href="mailto:cleionesio@gmail.com">Email</a></p>
-       <p><a href="https://www.linkedin.com/in/cleionesio-ribeiro-992615163/">LinkDin </a></p>
+       <p> <a href="mailto:cleionesio@gmail.com">Email</a><br/>cleionesio@gmail.com</p>
+       <p>
+         <a href="https://www.linkedin.com/in/cleionesio-ribeiro-992615163/">LinkDin </a><br/>
+          www.linkedin.com/in/cleionesio-ribeiro-992615163/
+        </p>
 
-       <p><a href="https://www.linkedin.com/in/cleionesio-ribeiro-992615163/">Git Hub </a></p>
+       <p><a href="https://www.linkedin.com/in/cleionesio-ribeiro-992615163/">Git Hub </a><br/>
+        https://www.github.com/Cleionesio2020</p>
         
         
       </div>
